1265dd1c2fdf9bd33ad4d5689a36c5892af0fe1b
Ervin Alexander Porter <mr.code.porter@gmail.com>
2023-06-05 13:47:50 -0400

    Add files via upload

---
+++ Get-DisabledUserDate.ps1
@@ -0,0 +1,29 @@
+﻿function Get-DisabledUserDate {
+    # Clear the console
+    Clear-Host
+
+    # Import the Active Directory module
+    Import-Module ActiveDirectory > $null
+
+    # Ask for the username to query
+    $userName = Read-Host -Prompt 'Input the username'
+
+    # Get the user object from Active Directory
+    $userObject = Get-ADUser -Identity $userName -Properties whenChanged, GivenName, Surname, Enabled 2> $null
+
+    # Check if the account is disabled
+    if ($userObject.Enabled -eq $false) {
+        # Output the user's first name, last name, and the date the account was disabled
+        $result = New-Object PSObject -Property @{
+            FirstName = $userObject.GivenName
+            LastName = $userObject.Surname
+            DateDisabled = $userObject.whenChanged
+        }
+        $result | Format-Table -AutoSize
+    } else {
+        Write-Output "Account is not disabled or does not exist."
+    }
+}
+
+# Call the function
+Get-DisabledUserDate
